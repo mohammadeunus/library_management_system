4e2039f41b1b07a69506160cb52b9eedd308726d
mohammad eunus <eunusur.rahaman.cse@ulab.edu.bd>
2022-04-23 15:42:10 +0600

    adminPage_managerList done

--- library_management_system/forms/adminPage_ManagerList.Designer.cs
+++ library_management_system/forms/adminPage_ManagerList.Designer.cs
@@ -29,38 +29,54 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(257, 9);
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(195, 51);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.Size = new System.Drawing.Size(245, 27);
             this.label1.TabIndex = 1;
-            this.label1.Text = "AdminPage_managerList";
+            this.label1.Text = "AdminPage: Manager\'s List";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // listView1
+            // dataGridView1
             // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(44, 44);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(541, 357);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(571, 227);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // refresh
+            // 
+            this.refresh.Font = new System.Drawing.Font("Segoe Script", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.refresh.Location = new System.Drawing.Point(34, 337);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(80, 27);
+            this.refresh.TabIndex = 4;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
             // 
             // adminPage_ManagerList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(638, 450);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "adminPage_ManagerList";
             this.Text = "adminPageManagerList";
+            this.Load += new System.EventHandler(this.adminPage_ManagerList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +85,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button refresh;
     }
 }
--- library_management_system/forms/adminPage_ManagerList.cs
+++ library_management_system/forms/adminPage_ManagerList.cs
@@ -7,19 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace library_management_system.forms
 {
     public partial class adminPage_ManagerList : Form
     {
+        string querry;
         public adminPage_ManagerList()
         {
             InitializeComponent();
+            datashow();
+        }
+        private void datashow()
+        {
+            Connection CN = new Connection();
+            querry = "SELECT * FROM MANAGERLOGININFO ";
+
+            SqlDataAdapter sda = new SqlDataAdapter(querry, CN.thisConnection);
+
+            DataTable ftable = new DataTable();
+            sda.Fill(ftable);
+            dataGridView1.DataSource=ftable;
+            CN.thisConnection.Close();
+
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void adminPage_ManagerList_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            datashow();
         }
     }
 }
